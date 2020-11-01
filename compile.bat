380c4e476ab03c3185e087404266500f6af8ba86
Anthony Rufrano <paradoxnj@comcast.net>
2020-10-31 22:40:04 -0400

    Render fullscreen 8 Bit Bitmap

---
+++ compile.bat
@@ -0,0 +1,28 @@
+@echo off
+
+SET PRGNAME=X16-METROID
+
+@echo Cleaning up
+if exist .\bin\*.prg del /q .\bin\*.prg
+if exist .\bin\*.sym del /q .\bin\*.sym
+if exist *.txt del /q *.txt
+::if exist *.x16 del /q *.x16
+::if exist *-x16.pal del /q *-x16.pal
+::if exist *.map del /q *.map
+
+if exist ..\..\emulator\%PRGNAME%.prg del /q ..\..\emulator\%PRGNAME%.prg
+
+@echo Compiling sources
+::..\..\tools\acme\acme.exe -f cbm --cpu 65c02 -o %PRGNAME%.PRG -l %PRGNAME%.lst %PRGNAME%.asm
+java -jar ..\..\tools\KickAssembler\KickAss.jar -debug -bytedump -showmem -maxAddr 131072 -odir .\bin x16-metroid.asm 
+if not exist .\bin\%PRGNAME%.prg goto END
+
+@echo Copy prg to emulator
+if exist .\bin\%PRGNAME%.prg copy .\bin\*.prg ..\..\emulator /y
+::if exist *.x16 copy *.x16 ..\..\emulator /y
+::if exist *.pal copy *.pal ..\..\emulator /y
+
+@echo Run the program in the emulator
+if exist ..\..\emulator\%PRGNAME%.prg ..\..\emulator\x16emu.exe -debug -run -prg ..\..\emulator\%PRGNAME%.prg -ram 2048 
+
+:END
---
+++ x16-mvclone.asm
@@ -0,0 +1,53 @@
+/*********************************************************************
+; Project:          Metroid Clone for Commander X16
+; File:             x16-metroid.asm
+; Author:           Anthony Rufrano (paradoxnj)
+; Description:      Main entry point
+**********************************************************************/
+.cpu _65c02
+
+BasicUpstart2(metroid_main)
+
+#import "x16.inc"
+
+.const VRAM_LOADMAP = $06000
+.const VRAM_TILEMAP = $10000
+.const VRAM_TILES   = $00000
+
+*=* "Program"
+
+metroid_main: {
+        stz CX16.VERA_ctrl
+
+        loadVRAM(bmp_pal, bmp_pal_end-bmp_pal, CX16.VRAM_palette)
+        loadVRAM(bmp_file, bmp_file_end-bmp_file, VRAM_TILES)
+
+        lda #%00010001
+        sta CX16.VERA_dc_video
+
+        lda #64
+        sta CX16.VERA_dc_hscale
+        sta CX16.VERA_dc_vscale
+
+        lda #%00000111
+        sta CX16.VERA_L0_config
+
+        stz CX16.VERA_L0_mapbase
+        stz CX16.VERA_L0_tilebase
+        stz CX16.VERA_L0_hscroll_l
+        stz CX16.VERA_L0_hscroll_h
+        stz CX16.VERA_L0_vscroll_l
+        stz CX16.VERA_L0_vscroll_h
+
+        jmp *
+        rts
+}
+
+
+bmp_file:       
+    .text "FF1TS1.X16"
+bmp_file_end:
+
+bmp_pal:
+    .text "FF1TS1-X16.PAL"
+bmp_pal_end:
