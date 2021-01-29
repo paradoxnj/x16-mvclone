31e196ca853882dfad255e5812d24f19e6fc5c9e
Anthony Rufrano <paradoxnj@comcast.net>
2021-01-29 18:00:09 -0500

    Got tiles rendering and layers working

--- x16-mvclone.asm
+++ x16-mvclone.asm
@@ -10,9 +10,10 @@ BasicUpstart2(mvclone_main)
 
 #import "x16.inc"
 
-.const VRAM_LOADMAP = $06000
+.const VRAM_L0MAP   = $06000
 .const VRAM_TILEMAP = $10000
 .const VRAM_TILES   = $00000
+.const VRAM_L1MAP   = $16000
 
 *=* "Program"
 
@@ -22,31 +23,60 @@ mvclone_main: {
         loadVRAM(bmp_pal, bmp_pal_end-bmp_pal, CX16.VRAM_palette)
         loadVRAM(bmp_file, bmp_file_end-bmp_file, VRAM_TILES)
 
-        lda #%00010001
+        loadVRAM(l0_map_file, l0_map_file_end-l0_map_file, VRAM_L0MAP)
+        loadVRAM(l1_map_file, l1_map_file_end-l1_map_file, VRAM_L1MAP)
+
+        lda #%00110001
         sta CX16.VERA_dc_video
 
         lda #64
         sta CX16.VERA_dc_hscale
         sta CX16.VERA_dc_vscale
 
-        lda #%00000111
+        lda #%01010011
         sta CX16.VERA_L0_config
 
-        stz CX16.VERA_L0_mapbase
-        stz CX16.VERA_L0_tilebase
+        lda #((VRAM_L0MAP >> 9) & $FF)
+        sta CX16.VERA_L0_mapbase
+
+        lda #((((VRAM_TILES >> 11) & $3F) << 2) | $03)
+        sta CX16.VERA_L0_tilebase
+        
         stz CX16.VERA_L0_hscroll_l
         stz CX16.VERA_L0_hscroll_h
         stz CX16.VERA_L0_vscroll_l
         stz CX16.VERA_L0_vscroll_h
+
+        lda #%01010011
+        sta CX16.VERA_L1_config
+
+        lda #((VRAM_L1MAP >> 9) & $FF)
+        sta CX16.VERA_L1_mapbase
+
+        lda #((((VRAM_TILES >> 11) & $3F) << 2) | $03)
+        sta CX16.VERA_L1_tilebase
+        
+        stz CX16.VERA_L1_hscroll_l
+        stz CX16.VERA_L1_hscroll_h
+        stz CX16.VERA_L1_vscroll_l
+        stz CX16.VERA_L1_vscroll_h
 
         jmp *
         rts
 }
 
 bmp_file:       
-    .text "FF1TS1.X16"
+    .text "POKEMON.X16"
 bmp_file_end:
 
 bmp_pal:
-    .text "FF1TS1-X16.PAL"
+    .text "POKEMON.PAL"
 bmp_pal_end:
+
+l0_map_file:
+    .text "POKETEST-0.MAP"
+l0_map_file_end:
+
+l1_map_file:
+    .text "POKETEST-1.MAP"
+l1_map_file_end:
